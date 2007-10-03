94de6fd61932bf4303c0a87134005af503d6f078
pfmantecon <pfmantecon@504931c3-773a-0410-b49c-77c20b5a621a>
2007-10-03 21:42:53 +0000

    Instancia el tablero, pide coordenadas destino y mueve el alfil.

--- trunk/ajedrez/src/java/clases/Main.java
+++ trunk/ajedrez/src/java/clases/Main.java
@@ -24,7 +24,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) { 
+    public static void main(String[] args){ 
         
         System.out.println("Bienvenido al Ajedrez");
         //Se define el array que pertenecera al Tablero
@@ -37,6 +37,34 @@ public class Main {
         
         //Flujo para leer datos
         InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br= new BufferedReader(isr);
+        
+        System.out.println("Solo puede mover el alfil blanco. Indique la fila.");
+        String fila="", columna="";
+        try {
+            
+            //Leemos la fila destino
+            fila = br.readLine();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        try {
+            //Leemos la columna destino
+            columna = br.readLine();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //Pasamos a enteros y restamos 1 para que corresponda con los indices
+        int fil = Integer.parseInt(fila);
+        int col = Integer.parseInt(columna);
+        fil--;
+        col--;
+        //Movemos la ficha, del origen (obligatorio) al destino dado por el usuario
+        tablero.mover(0,2,fil,col,tablero);
+        
+        
+        
         
         
 	    }

bf350f173a6698b7c18bdb518ed4c77f27de0adb
javierhertfelder <javierhertfelder@504931c3-773a-0410-b49c-77c20b5a621a>
2007-10-21 19:53:58 +0000

    WebAjedrez en una rama

--- ajedrez/docs/BD ajedrez.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `usuario` (
-  `id_usuario` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `pass` blob NOT NULL,
-  /*`fecha_registro` date NULL,*/
-  `nick` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;

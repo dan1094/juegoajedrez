39ee4770cb937621136fd90a27065afa169dd99b
pfmantecon <pfmantecon@504931c3-773a-0410-b49c-77c20b5a621a>
2007-10-31 10:57:32 +0000

    Clase FIDE añadida.

---
+++ trunk/ajedrez/src/java/controller/Fide.java
@@ -0,0 +1,28 @@
+/*
+ * Fide.java
+ *
+ * Created on 31 de octubre de 2007, 11:55
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import model.partida.*;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Fide {
+    
+    /** Creates a new instance of Fide */
+    public Fide() {
+    }
+    
+    public int[] de_fide_a_modelo(Partida partida, String mov_en_fide){
+        
+    }
+    
+}

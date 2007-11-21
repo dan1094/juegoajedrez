59aadd461aeae77cf40f75a48f89283e4ed5977d
javierhertfelder <javierhertfelder@504931c3-773a-0410-b49c-77c20b5a621a>
2007-11-21 18:41:34 +0000

    Añadido crear vistas y eliminarlas aunque no funciona muy bien

---
+++ ajedrez/src/java/view/MostrarTableroBlanco.java
@@ -0,0 +1,41 @@
+/*
+ * MostrarTableroBlanco.java
+ *
+ * Created on 23 de octubre de 2007, 12:18
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package view;
+
+import model.partida.Partida;
+import org.me.hello.*;
+
+
+
+/**
+ *
+ * @author Administrador
+ */
+public class MostrarTableroBlanco extends MostrarTablero{
+    
+    /**
+     * Creates a new instance of MostrarTableroBlanco
+     */
+    private  VistaTablero VistaTablero;
+    
+    public MostrarTableroBlanco() {
+        
+        //VistaTablero = new VistaTablero();
+    }
+    public void update(Partida partida)
+    {
+        VistaTableroPieza vistaTableroPieza = new VistaTableroPieza();
+             
+        vistaTableroPieza.init(vistaTableroPieza);
+             
+    }
+   
+    
+}
